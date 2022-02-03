87d19f0997599d85689077200b7b50759e4dc41b
bbgatch <10412365+bbgatch@users.noreply.github.com>
2022-02-02 23:42:30 -0500

    Updating TSA plot and site formatting.

--- projects/tsa/plot_data.py
+++ projects/tsa/plot_data.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('tsa.csv')
 
+df['Date'] = pd.to_datetime(df['Date'])
+
+
+# Plot data
+plt.figure(figsize=(8, 5))
 plt.plot(df['Date'], df['Passengers'])
+plt.title('U.S. TSA Checkpoint Passengers | data through ' + max(df['Date']).strftime('%Y-%m-%d'))
+
 plt.savefig('tsa.png')
 # plt.show()
+
