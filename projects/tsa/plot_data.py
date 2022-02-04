1fd175219ec6a157149995eedc660e48d3f91703
bbgatch <10412365+bbgatch@users.noreply.github.com>
2022-02-03 23:23:13 -0500

    Make image size responsive.

--- projects/tsa/plot_data.py
+++ projects/tsa/plot_data.py
@@ -5,12 +5,10 @@ df = pd.read_csv('tsa.csv')
 
 df['Date'] = pd.to_datetime(df['Date'])
 
-
 # Plot data
-plt.figure(figsize=(8, 5))
-plt.plot(df['Date'], df['Passengers'])
+plt.figure(figsize=(9, 5))
+plt.plot(df['Date'], df['Passengers'], color='darkslategray')
 plt.title('U.S. TSA Checkpoint Passengers | data through ' + max(df['Date']).strftime('%Y-%m-%d'))
 
 plt.savefig('tsa.png')
-# plt.show()
-
+plt.show()
