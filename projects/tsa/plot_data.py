ce2936c752845adf1195649f3ac581b4e2e88dfb
bbgatch <10412365+bbgatch@users.noreply.github.com>
2022-02-18 19:24:02 -0500

    Adding plot of TSA data by year.

--- projects/tsa/plot_data.py
+++ projects/tsa/plot_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 df = pd.read_csv('tsa.csv')
 df['Date'] = pd.to_datetime(df['Date'])
@@ -15,4 +16,27 @@ def line_plot_full(df):
 
 line_plot_full(df)
 
+
+# Plot data by year
+df = pd.read_csv('tsa-orig.csv')
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Get year labels from columns
+years = df.columns[1:].sort_values()
+num_years = len(years)
+
+# Need to figure out the coloring so that it makes sense.
+# Create colors for each year
+cmap = cm.get_cmap('Blues', 10)
+colors = cmap(range(num_years))
+colors = cmap(range(10))
+
+plt.figure(figsize=(9, 5))
+for i in range(num_years):
+    plt.plot(df['Date'],
+             df[years[i]],
+             label=years[i],
+             color=colors[-i])
+plt.legend()
+plt.show()
 print('Data plotted.')
