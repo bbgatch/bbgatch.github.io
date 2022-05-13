d507349fa85ef04e7bcc7b0934509e39df6d39e9
Ben Gatchell <10412365+bbgatch@users.noreply.github.com>
2022-05-12 20:48:56 -0400

    Add forecast code. Update TSA data.

--- projects/tsa/forecasting-with-fable/forecast.R
+++ projects/tsa/forecasting-with-fable/forecast.R
@@ -1,7 +1,8 @@
-# Data manipulation
+# Data manipulation and visualization
 library(readr)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 # Time series analysis
 library(tsibble)
 library(fable)
@@ -9,15 +10,20 @@ library(feasts)
 
 # Read in data
 df <- read_csv('../data/tsa-backcast.csv')
+# df <- df |> mutate(YearWeek = yearweek(Date)) |>
 df <- df |> mutate(Date = yearmonth(Date)) |>
     group_by(Date) |>
     summarise(Passengers = sum(Passengers)) |>
     as_tsibble(index = Date)
 
-df <- df |> filter(Date < yearmonth(today()))
+df <- df |> as_tsibble(index = Date)
+
+# df <- df |> filter(YearWeek < yearweek(today()))
+# df <- df |> filter(Date < yearmonth(today()))
 autoplot(df)
 
-df_covid <- df |> filter(Date >= yearmonth("2020 Apr"))
+# df_covid <- df |> filter(Date >= yearmonth("2020 Apr"))
+# df <- df |> filter(YearWeek >= yearweek("2020-04-01"))
 
 fcst <- df |> model(
     ets = ETS(Passengers),
@@ -25,16 +31,24 @@ fcst <- df |> model(
     ets_log = ETS(log(Passengers)),
     arima_log = ARIMA(log(Passengers), stepwise = FALSE, approximation = FALSE)
     ) |>
-    mutate(combo = (ets + arima) / 2) |>
-    forecast(h = 9)
+    mutate(
+        combo = (ets + arima) / 2,
+        combo_log = (ets_log + arima_log) / 2
+        ) |>
+    forecast(h = 7)
 
-autoplot(fcst, df)
+fcst
+
+autoplot(fcst, filter(df, Date >= today() - 21))
+autoplot(fcst, filter(df, Date >= today() - 21), level = NULL)
+
+# autoplot(fcst, df)
 autoplot(fcst, df, level = NULL)
     
 
 # Combine data and forecast
 fcst <- fcst |>
-    filter(.model == 'combo') |>
+    filter(.model == 'combo_log') |>
     as_tibble() |>
     select(Date, .mean) |>
     rename(Passengers = .mean)
