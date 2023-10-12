f50c8b1fc81df79f52b9ca651384004ed0cf6523
bbgatch <10412365+bbgatch@users.noreply.github.com>
2023-10-11 22:49:16 -0400

    Start work on updating TSA code.

---
+++ projects/tsa/code/pull-tsa-data.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+from io import StringIO
+
+'''
+Pull TSA checkpoint traveler data from
+https://www.tsa.gov/coronavirus/passenger-throughput
+'''
+# Guided by:
+# https://towardsdatascience.com/a-guide-to-scraping-html-tables-with-pandas-and-beautifulsoup-7fc24c331cf7
+# https://stackoverflow.com/questions/43590153/http-error-403-forbidden-when-reading-html
+
+# URL that we want to pull TSA data from
+url = "http://www.tsa.gov/travel/passenger-volumes"
+
+# Read the page using requests.get()
+r = requests.get(url)
+
+# Use pd.read_html() to parse the html text
+df = pd.read_html(StringIO(r.text))
+
+# The result is a list of 1 dataframe, we need to select that dataframe from the list
+df = df[0]
+
+# Changing data types.
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Sorting by Date.
+df = df.sort_values(by='Date')
+
+# Save data in original format
+df.to_csv('data/tsa-orig.csv', index=False)
+print('Data pulled.')
