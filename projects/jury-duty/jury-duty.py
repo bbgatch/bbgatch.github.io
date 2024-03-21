7081d168589ded657cb128c3fa1416b944964ca3
bbgatch <10412365+bbgatch@users.noreply.github.com>
2024-03-20 22:57:04 -0400

    Add to jury duty post

--- projects/jury-duty/jury-duty.py
+++ projects/jury-duty/jury-duty.py
@@ -6,12 +6,10 @@ import matplotlib.pyplot as plt
 sql = """
     select
         group_num
-        -- ,datepart('dayofweek', date) as dow
-        -- ,dayname(date) as day
         ,sum(called) as days_called
         ,count(called) as days_summoned
-        -- ,sum(called) / count(called) as call_pct
-        ,cast(round((sum(called) / count(called)) * 100, 2) as string) || '%' as call_pct_str
+        ,sum(called) / count(called) as call_probability
+        ,cast(round((sum(called) / count(called)) * 100, 2) as string) || '%' as call_probability_str
 
     from read_csv('jury-duty.csv')
     group by 1
@@ -21,14 +19,11 @@ sql = """
 duckdb.sql(sql)
 df = duckdb.sql(sql).df()
 
-# Plot group call percentage
+# Plot group call probability
 fig, ax = plt.subplots()
-ax = sns.barplot(df, x="group_num", y="call_pct")
+ax = sns.barplot(df, x="group_num", y="call_probability")
 ax.bar_label(ax.containers[0], fmt=lambda x: f"{x * 100:.1f}%")
-ax.set(xlabel="Group Number", ylabel="Call Percentage", title="Jury Duty Group Call Percentage")
+ax.set(xlabel="Group Number", ylabel="Call Probability", title="Jury Duty Group Call Probability")
 ax.yaxis.set_major_formatter(mpl.ticker.PercentFormatter(xmax=1, decimals=0))
 plt.savefig('images/group-probability-plot.png')
-
-min_date = duckdb.sql("select min(date) from df").fetchone()[0].strftime('%Y-%m-%d')
-max_date = duckdb.sql("select max(date) from df").fetchone()[0].strftime('%Y-%m-%d')
 
