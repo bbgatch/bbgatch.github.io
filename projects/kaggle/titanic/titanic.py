cb5cc77c62196821cde9a6eaaa3382f2725c65bd
bbgatch <10412365+bbgatch@users.noreply.github.com>
2025-02-03 22:01:55 -0500

    Working on Titanic

--- projects/kaggle/titanic/titanic.py
+++ projects/kaggle/titanic/titanic.py
@@ -1,4 +1,4 @@
-import numpy as np
+import duckdb
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -10,6 +10,42 @@ from sklearn.metrics import accuracy_score
 # Read in train/test data
 train = pd.read_csv("data/train.csv")
 test = pd.read_csv("data/test.csv")
+
+duckdb.sql("""
+select
+    -- case
+    --     when lower(name) like '%mrs%' then 'mrs'
+    --     when lower(name) like '%miss%' then 'miss'
+    --     when lower(name) like '%rev%' then 'rev'
+    --     else 'other'
+    -- end as title
+    -- case when cabin is null then null else 'cabin' end as cabin
+    ticket
+    ,regexp_extract(ticket, '\d+') as ticket_number
+    ,avg(survived) as survival_rate
+        
+from train
+group by 1, 2
+""")
+
+
+# Combine train and test for reviewing values
+df = pd.concat([train, test])
+df['Cabin'].unique()
+df['Cabin'].unique()
+
+df['Cabin'] = df['Cabin'].str.replace('F ', '')
+
+
+with pd.option_context('display.max_rows', 1000):
+    print(df['Ticket'].sort_values(ascending=False))
+
+print(train[['Ticket', 'Survived']].sort_values(by='Ticket', ascending=False).to_string(max_rows=1000))
+print(train[['Name']].sort_values(by='Name', ascending=False).to_string(max_rows=1000))
+
+categorical_features = ['Sex', 'Embarked']
+numeric_features = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+set(list(df.columns)).symmetric_difference(categorical_features + numeric_features)
 
 # Look for nulls
 train.drop(columns=['Survived', 'PassengerId', 'Name']).isna().sum()
@@ -53,13 +89,13 @@ output.to_csv('data/submission.csv', index=False)
 print("Submission saved successfully!")
 
 # Check if survival rate varies by cabin letter
-# train.groupby(train['Cabin'].str[0]).agg({
-#     'PassengerId': 'count',
-#     'Survived': 'mean'
-# }).rename(columns={
-#     'PassengerId': 'Count',
-#     'Survived': 'SurvivalRate'
-# })
+train.groupby(train['Cabin'].str[0]).agg({
+    'PassengerId': 'count',
+    'Survived': 'mean'
+}).rename(columns={
+    'PassengerId': 'Count',
+    'Survived': 'SurvivalRate'
+})
 
 # train.groupby(train['Cabin'].str[1:]).agg({
 #     'PassengerId': 'count',
