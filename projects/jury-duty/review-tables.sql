caa0caa422e17e2ef474bfe5910c4e1aae6f18d5
bbgatch <10412365+bbgatch@users.noreply.github.com>
2025-03-12 22:32:07 -0400

    Update review-tables.sql

--- projects/jury-duty/review-tables.sql
+++ projects/jury-duty/review-tables.sql
@@ -7,7 +7,9 @@ select
     ,status
     ,call_sid
 from recordings
-order by date_created asc;
+order by date_created desc
+limit 5
+;
 
 select
     sid
@@ -18,4 +20,6 @@ select
     ,status
     ,transcription_text
 from transcriptions
-order by date_created asc;
+order by date_created desc
+limit 5
+;
